4c0cef5efebbf07588041b724b8712e31c331866
Risstats <larissais@icloud.com>
2022-04-06 21:49:59 -0400

    Initial commit

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,13 @@
+#' strsplit1
+#'
+#' @param x A character vector with more than one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ToyPackage)
+
+test_check("ToyPackage")
